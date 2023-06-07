c61ad27d6002489d16aad13b4218f315532376b7
sky-boom <865735816@qq.com>
2023-06-07 21:58:47 +0800

    移动目录结构

--- src/main/java/com/wzr/rendisk/config/mvc/CorsConfig.java
+++ src/main/java/com/wzr/rendisk/config/mvc/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.wzr.rendisk.config;
+package com.wzr.rendisk.config.mvc;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
